2023588c7e24d5e05a013a704ba79bd73856496d
GODFATHER_py <drdonutdani@gmail.com>
2024-05-26 10:07:05 -0700

    setup.bat
    new start-up

--- setup.bat
+++ setup.bat
@@ -1,12 +1,11 @@
 @echo off
 echo Installing required Python packages...
-pip install PyQt5
-pip install pynput
-pip install pygetwindow
-pip install pyautogui
-pip install pyinstaller
+pip install PyQt5 pynput pygetwindow pyautogui pyinstaller >nul 2>&1
 
 echo Requirements installed. Launching DarkNet-Adware.py...
 python DarkNet-Adware.py
+
+echo Launching example.py...
+python example.py
 
 pause
